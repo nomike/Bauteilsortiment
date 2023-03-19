75fda8a7ddce4d01b6a60f50dcf70b24f90b75f1
nomike <nomike@nomike.com>
2023-03-19 04:47:47 +0100

    Code format.

--- bts/dk_info/models.py
+++ bts/dk_info/models.py
@@ -13,11 +13,13 @@ os.environ['DIGIKEY_CLIENT_SECRET'] = 'piWwjB6V8TYsxlLg'
 os.environ['DIGIKEY_CLIENT_SANDBOX'] = 'False'
 os.environ['DIGIKEY_STORAGE_PATH'] = './cache'
 
+
 class AssortmentBox(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
+
 
 class StorageUnitType(models.Model):
     name = models.CharField(max_length=255)
@@ -34,18 +36,20 @@ class StorageUnit(models.Model):
     name = models.CharField(max_length=255)
     number = models.IntegerField()
     assortment_box = models.ForeignKey(AssortmentBox, on_delete=models.CASCADE)
-    storage_unit_type = models.ForeignKey(StorageUnitType, on_delete=models.CASCADE, null=True)
+    storage_unit_type = models.ForeignKey(
+        StorageUnitType, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f'{self.assortment_box.name}/{self.number}'
+
 
 class StorageUnitCompartment(models.Model):
     name = models.CharField(max_length=255, null=True)
     storage_unit = models.ForeignKey(StorageUnit, on_delete=models.CASCADE)
 
-
     def __str__(self):
         return f'{self.storage_unit.assortment_box.name}/{self.storage_unit.number}/{self.name}'
+
 
 class Merchant(models.Model):
     name = models.CharField(max_length=254, null=True)
@@ -53,10 +57,13 @@ class Merchant(models.Model):
     def __str__(self):
         return self.name
 
+
 class Component(models.Model):
-    storage_unit_compartment = models.ForeignKey(StorageUnitCompartment, on_delete=models.CASCADE, null=True)
+    storage_unit_compartment = models.ForeignKey(
+        StorageUnitCompartment, on_delete=models.CASCADE, null=True)
     part_number = models.CharField(max_length=64, primary_key=True)
-    resell_price = models.DecimalField(null=True, max_digits=20, decimal_places=5)
+    resell_price = models.DecimalField(
+        null=True, max_digits=20, decimal_places=5)
     usual_order_quantity = models.IntegerField(default=1)
     primary_datasheet = models.CharField(max_length=254, null=True)
     detailed_description = models.CharField(max_length=254, null=True)
@@ -70,9 +77,10 @@ class Component(models.Model):
     def update_cache(self):
         raise NotImplementedError()
 
+
 class DigiKeyComponent(Component):
     cache_expiry = models.DateTimeField(default=timezone.now)
-   
+
     def update_cache(self):
         pd = digikey.product_details(self.part_number)
         self.primary_datasheet = pd.primary_datasheet
@@ -83,7 +91,8 @@ class DigiKeyComponent(Component):
                 self.order_unit_price = price.unit_price
         dt = timezone.now()
         dt = dt + datetime.timedelta(days=10)
-        self.cache_expiry=dt
+        self.cache_expiry = dt
+
 
 class Inventory(models.Model):
     component = models.ForeignKey(Component, on_delete=models.CASCADE)
@@ -93,6 +102,7 @@ class Inventory(models.Model):
     def __str__(self):
         return f'{self.component.part_number} ({self.count})'
 
+
 class Purchase(models.Model):
     merchant = models.ForeignKey(Merchant, on_delete=models.CASCADE)
     order_number = models.CharField(max_length=64)
@@ -100,6 +110,7 @@ class Purchase(models.Model):
 
     def __str__(self):
         return f'{self.merchant.name} order number {self.order_number} from {self.timestamp}'
+
 
 class PurchaseLine(models.Model):
     component = models.ForeignKey(Component, on_delete=models.CASCADE)
@@ -109,5 +120,3 @@ class PurchaseLine(models.Model):
 
     def __str__(self):
         return f'{self.purchase.merchant.name} order number {self.purchase.order_number}: {self.component.part_number}'
-
-    
--- bts/dk_info/urls.py
+++ bts/dk_info/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('component/<str:dk_part_number>', views.component, name='component_details'),
-    path('digikeycomponent/<str:dk_part_number>/update_cache', views.digikey_component_update_cache, name='digikey_component_update_cache'),
+    path('component/<str:dk_part_number>',
+         views.component, name='component_details'),
+    path('digikeycomponent/<str:dk_part_number>/update_cache',
+         views.digikey_component_update_cache, name='digikey_component_update_cache'),
     path('component/', views.components, name='components'),
 ]
