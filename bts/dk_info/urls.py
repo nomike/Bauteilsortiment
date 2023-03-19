6570e957bff39bf8f6c0f289812228302883cb31
nomike <nomike@nomike.com>
2023-03-19 05:04:03 +0100

    Add URLs for Component, and DigikeyComponent. Move update_cache URL to Component namespace.

--- bts/dk_info/urls.py
+++ bts/dk_info/urls.py
@@ -4,9 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('component/', views.components, name='components'),
     path('component/<str:dk_part_number>',
          views.component, name='component_details'),
-    path('digikeycomponent/<str:dk_part_number>/update_cache',
+    path('digikeycomponent/', views.components, name='components'),
+    path('digikeycomponent/<str:dk_part_number>',
+         views.component, name='component_details'),
+    path('component/<str:dk_part_number>/update_cache',
          views.digikey_component_update_cache, name='digikey_component_update_cache'),
-    path('component/', views.components, name='components'),
 ]
