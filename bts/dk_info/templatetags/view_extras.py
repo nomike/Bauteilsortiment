6ceb8fd8aad30d4fad7282d86093b0e4967671af
nomike <nomike@nomike.com>
2023-03-20 21:23:26 +0100

    Add view_extras templatetag module.

---
+++ bts/dk_info/templatetags/view_extras.py
@@ -0,0 +1,14 @@
+from django.template import Variable, VariableDoesNotExist, Library
+
+
+register = Library()
+
+
+@register.filter
+def hash(object, attr):
+    pseudo_context = {'object': object}
+    try:
+        value = Variable('object.%s' % attr).resolve(pseudo_context)
+    except VariableDoesNotExist:
+        value = None
+    return value
