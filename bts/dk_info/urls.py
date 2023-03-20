c99ab0dc134b8dd344965d8ec660b943c707a463
nomike <nomike@nomike.com>
2023-03-20 21:24:04 +0100

    Add generic templates.

--- bts/dk_info/urls.py
+++ bts/dk_info/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('component/', views.components, name='components'),
-    path('component/<int:component_id>',
-         views.component, name='component_details'),
-    path('component/<int:component_id>/update_cache',
-         views.component_update_cache, name='component_update_cache'),
+    path('generic/Component', views.ComponentListView.as_view(),
+         name="component_list"),
+    path('generic/Component/<int:pk>', views.ComponentDetailView.as_view(),
+         name="component_detail"),
+    #     path('component/<int:component_id>/update_cache',
+    #          views.component_update_cache, name='component_update_cache'),
 ]
--- bts/dk_info/views.py
+++ bts/dk_info/views.py
@@ -1,30 +1,11 @@
-from django.forms.models import model_to_dict
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from django.urls import reverse
+from django.views.generic import DetailView, ListView
 from dk_info.models import Component
 
 # Create your views here.
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-
-def components(request):
-    context = {'components': Component.objects.all()}
-    return render(request, 'Components/index.html', context)
-
-
-def component(request, component_id):
-    component = get_object_or_404(Component, pk=component_id)
-
-    context = {
-        'component': component,
-        'fields': model_to_dict(component, exclude=['id']).items()
-    }
-
-    return render(request, 'Component/index.html', context)
 
 
 def component_update_cache(request, component_id):
@@ -33,3 +14,17 @@ def component_update_cache(request, component_id):
     component.save()
 
     return HttpResponseRedirect(reverse('component_details', args=(component.id,)))
+
+
+class ComponentListView(ListView):
+    model = Component
+    context_object_name = "all_components"
+
+
+class ComponentDetailView(DetailView):
+    model = Component
+    context_object_name = "all_components"
+
+    def get_queryset(self):
+        self.component = get_object_or_404(self.model, pk=self.kwargs['pk'])
+        return Component.objects.filter(pk=self.component.pk)
