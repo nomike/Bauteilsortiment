48304d32908e335b2f6cfcd332770df99bd1516c
nomike <nomike@nomike.com>
2023-03-22 23:31:35 +0100

    rename main module from bts to bauteilsortiment.

--- bauteilsortiment/asgi.py
+++ bauteilsortiment/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for bts project.
+ASGI config for bauteilsortiment project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bts.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bauteilsortiment.settings')
 
 application = get_asgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bts.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'bauteilsortiment.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
