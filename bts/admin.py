e1b397beb823bf4bac18f6793729aac38f33b668
nomike <nomike@nomike.com>
2023-03-29 01:15:03 +0200

    Add MultiLevelSelect custom widget and use it for InventoryAdmin.storage_unit_compartment.

--- bts/admin.py
+++ bts/admin.py
@@ -4,13 +4,37 @@ from .models import (AssortmentBox, Category, Component, ComponentType, SubCompo
                      Merchant, Purchase, PurchaseLine, StorageUnit,
                      StorageUnitCompartment, StorageUnitType)
 
+from django.forms.widgets import Select
+from django import forms
+
+
+class MultiLevelSelect(Select):
+
+    def __init__(self):
+        super().__init__()
+        self.template_name = "bts/multilevelselect/select.html"
+
 
 class SubComponentAdmin(admin.ModelAdmin):
     search_fields = ["name", "component_type"]
 
 
+class InventoryAdminForm(forms.ModelForm):
+
+    storage_unit_compartment = MultiLevelSelect()
+
+    class Meta:
+        fields = ["sub_component",
+                  "storage_unit_compartment", "timestamp", "count"]
+        model = Inventory
+        widgets = {
+            'storage_unit_compartment': MultiLevelSelect
+        }
+
+
 class InventoryAdmin(admin.ModelAdmin):
     autocomplete_fields = ["sub_component"]
+    form = InventoryAdminForm
 
 
 # Register your models here.
