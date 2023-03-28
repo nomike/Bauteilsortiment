64c051ed85e4c2e40e3eb5b2dfde50a3e61ae498
nomike <nomike@nomike.com>
2023-03-29 00:47:51 +0200

    Make "sub_component" field searchable in InventoryAdmin.

--- bts/admin.py
+++ bts/admin.py
@@ -4,16 +4,25 @@ from .models import (AssortmentBox, Category, Component, ComponentType, SubCompo
                      Merchant, Purchase, PurchaseLine, StorageUnit,
                      StorageUnitCompartment, StorageUnitType)
 
+
+class SubComponentAdmin(admin.ModelAdmin):
+    search_fields = ["name", "component_type"]
+
+
+class InventoryAdmin(admin.ModelAdmin):
+    autocomplete_fields = ["sub_component"]
+
+
 # Register your models here.
 admin.site.register(AssortmentBox)
 admin.site.register(Category)
 admin.site.register(ComponentType)
 admin.site.register(Component)
-admin.site.register(Inventory)
+admin.site.register(Inventory, InventoryAdmin)
 admin.site.register(Merchant)
 admin.site.register(Purchase)
 admin.site.register(PurchaseLine)
 admin.site.register(StorageUnit)
 admin.site.register(StorageUnitCompartment)
 admin.site.register(StorageUnitType)
-admin.site.register(SubComponent)
+admin.site.register(SubComponent, SubComponentAdmin)
