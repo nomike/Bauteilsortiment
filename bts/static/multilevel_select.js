3e52465485c0e9bdad6e372dd284368ec0cf4a3a
nomike <nomike.postmann@paysafe.com>
2023-04-05 00:53:02 +0200

    Cleanup: Remove debug statements

--- bts/static/multilevel_select.js
+++ bts/static/multilevel_select.js
@@ -34,7 +34,6 @@ $(document).ready(function () {
  * @param {Number} select_id The index of the select
  */
 function clear_select(select_id) {
-    console.debug(`clear_select(${select_id})`);
     $(fields[select_id]['id']).empty();
 }
 
@@ -44,15 +43,11 @@ function clear_select(select_id) {
  * @param {Number} select_id The index of the select
  */
 function handle_change(select_id) {
-    console.debug(`Handling change event for select_id=${select_id} (value=${$(fields[select_id]['id']).val()})`);
-
     const select_number = select_id + 1;
     for (let i = select_number; i < fields.length; i++) {
-        console.debug(`Emptying select ${fields[i]['id']}`);
         clear_select(i);
     }
     if (select_number < fields.length) {
-        console.debug(`Fill select ${select_number}`);
         fill_select(select_number);
     }
 }
@@ -69,14 +64,12 @@ function fill_select(select_id) {
     const filter_model = select_id > 0 ? fields[select_id - 1]['model'] : null;
     const id = select_id > 0 ? $(fields[select_id - 1]['id']).val() : null;
 
-    console.debug(`fill_select(select_id=${select_id}); // select=${select}, model=${model}, display_field=${display_field}, filter_model=${filter_model}, id=${id})`);
     let url;
     if (filter_model != null) {
         url = '/bts/json/' + model + '/' + filter_model + '/' + id;
     } else {
         url = '/bts/json/' + model;
     }
-    console.debug(`URL=${url}`);
 
     $.ajax({
         url: url,
@@ -85,10 +78,7 @@ function fill_select(select_id) {
         success: function (data) {
             let count = 0;
             $.each(data, function () {
-                console.debug(`###COUNT=${count++}`);
-                console.debug(`Appending option ${this[display_field]} to select {select_id}`);
                 select.append($("<option />").val(this.id).text(this[display_field]));
-                console.debug("Done getting JSON");
                 log_list_value(select_id, `done`);
             });
         }
@@ -130,6 +120,4 @@ function set_field(select_id, value) {
         fill_select(i);
         $(fields[i]['id']).val(selected_objects[i]).change();
     }
-
-    console.debug(`## selected_objects=${selected_objects}`);
 } 
