ff3bcd0bb46615377d1780966c91def94631e0b3
nomike <nomike.postmann@paysafe.com>
2023-04-07 00:22:49 +0200

    Make urls shorter so they fit better inside qrcodes.

--- bts/urls.py
+++ bts/urls.py
@@ -12,17 +12,17 @@ urlpatterns = []
 for name in [obj.__name__ for name, obj in bts.models.__dict__.items()
              if inspect.isclass(obj) and issubclass(obj, models.Model)]:
     # generic list
-    urlpatterns.append(path(f'generic/{name}', getattr(views,  f'{name}ListView').as_view(),
+    urlpatterns.append(path(f'm/{name}', getattr(views,  f'{name}ListView').as_view(),
                             name=f"{snake_case(name)}_list"))
-    urlpatterns.append(path(f'generic/{name}/', getattr(views,  f'{name}ListView').as_view(),
+    urlpatterns.append(path(f'm/{name}/', getattr(views,  f'{name}ListView').as_view(),
                             name=f"{snake_case(name)}_list"))
 
     # filtered list
-    urlpatterns.append(path(f'generic/{name}/filtered/<str:model>/<int:id>', getattr(views,  f'{name}FilteredListView').as_view(),
+    urlpatterns.append(path(f'm/{name}/filtered/<str:model>/<int:id>', getattr(views,  f'{name}FilteredListView').as_view(),
                             name=f"{snake_case(name)}_filtered_list"))
 
     # generic detail page
-    urlpatterns.append(path(f'generic/{name}/id/<int:pk>', getattr(views,  f'{name}DetailView').as_view(),
+    urlpatterns.append(path(f'm/{name}/id/<int:pk>', getattr(views,  f'{name}DetailView').as_view(),
                             name=f"{snake_case(name)}_detail"))
 
 
