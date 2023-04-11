4686eb8721a1c79d7e8b8960347659e268eac12b
nomike <nomike.postmann@paysafe.com>
2023-04-11 02:12:47 +0200

    [#1] Add docstrings to views.py.

--- bts/views.py
+++ bts/views.py
@@ -23,7 +23,7 @@ import qrcode
 import qrcode.image.svg
 from django.db import models
 from django.db.models import QuerySet
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse, HttpRequest
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import View
@@ -97,10 +97,16 @@ view_config = {
     },
 }
 
-# Create your views here.
-
 
 def home_view(request):
+    """Home view which lists all model cstegories.
+
+    Args:
+        request (HttpRequest): Django Request Object
+
+    Returns:
+        HTTPResponse: A HTML rendering of the home view.
+    """
     context = {
         "types": view_config.keys()
     }
@@ -116,7 +122,17 @@ def home_view(request):
 
 
 class ConfiguredListView(ListView):
+    """Abstract class for a configurable list view.
+    Shows a list of objects for a certain Model.
+    """
+
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        """Overloads super().get_context_data() to inject the view_config into the
+        template's context.
+
+        Returns:
+            Dict[str, Any]: The context used in the template.
+        """
         context_data = super().get_context_data(**kwargs)
         for key, value in view_config[self.model].items():
             context_data[key] = value
@@ -124,7 +140,17 @@ class ConfiguredListView(ListView):
 
 
 class ConfiguredDetailView(DetailView):
+    """Abstract class for a onfigurable detail view.
+    Renders a tabular detail page for one object, listing parameters and sub-lists (for foreign objects).
+    """
+
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        """Overloads super().get_context_data() to inject the view_config into the
+        template's context.
+
+        Returns:
+            Dict[str, Any]: The context used in the template.
+        """
         context_data = super().get_context_data(**kwargs)
         for key, value in view_config[self.model].items():
             context_data[key] = value
@@ -132,25 +158,61 @@ class ConfiguredDetailView(DetailView):
 
 
 def model_json_view(request, model: str):
+    """Renders a JSON document with all items of the specified model.
+
+    Args:
+        request (HttpRequest): Django Request Object
+        model (str): The name of the model you want to represent
+
+    Returns:
+        JsonResponse: The JSON document as a JsonResponse HttpResponse object.
+    """
     return JsonResponse(list(getattr(bts.models, model).objects.values()), safe=False)
 
 
-def model_json_filtered_view(View, model: str, filter_model: str, id: int):
+def model_json_filtered_view(request, model: str, filter_model: str, id: int):
+    """Renders a JSON document with all items of the specified model filtered by a foreign key.
+
+    Args:
+        request (HttpRequest): Django Request Object
+        model (str): The name of the model you want to represent
+        filter_model (str): The name of the model you want to filter by
+        id (int): The id you want to filter by
+
+    Returns:
+        JsonResponse: The JSON document as a JsonResponse HttpResponse object.
+    """
     data = getattr(bts.models, model).objects.filter(
         **{view_extras.snake_case(filter_model): id})
     return JsonResponse(list(data.values()), safe=False)
 
 
 def model_json_field_view(request, model: str, id: int, field: str):
+    """Renders a JSON document containing a single parameter of an ojbect.
+
+    Args:
+
+        request (HttpRequest): Django Request Object
+        model (str): The name of the model you want to represent
+        id (int): The object's ID
+        field (str): The fiels who's value you want to have
+
+    Returns:
+        JsonResponse: The JSON document as a JsonResponse HttpResponse object.
+    """
     return JsonResponse(getattr(get_object_or_404(getattr(bts.models, model), pk=id), field), safe=False)
 
 
 class ModelListView(ConfiguredListView):
+    """A generic list view.
+    """
     model = None
     template_name = "bts/model_list_page.html"
 
 
 class ModelFilteredListView(ConfiguredListView):
+    """A generic filtered list view.
+    """
     model = None
     template_name = "bts/model_list_snippet.html"
 
@@ -162,6 +224,8 @@ class ModelFilteredListView(ConfiguredListView):
 
 
 class ModelDetailView(ConfiguredDetailView):
+    """A generic detail view.
+    """
     model = None
     template_name = "bts/model_detail.html"
 
@@ -178,6 +242,16 @@ def storage_unit_label_html(request):
 
 
 def labelpage(request, id):
+    """Render a HTML page full of printable lables.
+    The lables could be put on the StorageUnits.
+
+    Args:
+        request (HttpRequest): Django Request Object
+        id (int): The object's ID
+
+    Returns:
+        HttpResponse: The rendered label page.
+    """
     assortment_box = get_object_or_404(AssortmentBox, pk=id)
     context = {"assortment_box": assortment_box,
                "storage_units": StorageUnit.objects.filter(assortment_box=assortment_box)}
@@ -185,6 +259,19 @@ def labelpage(request, id):
 
 
 def qr_code_svg(request, model, id):
+    """Renders a SVG image containing a qr code with a link to a specific object.
+
+    Args:
+        request (HttpRequest): Django Request Object
+        model (str): The name of the model you want to represent
+        id (int): The object's ID
+
+    Request arguments:
+        GET.box_size (int): Specify the size of one box of the QR code in pixels.
+
+    Returns:
+        HttpResponse: A response object containing the SVG image.
+    """
     box_size = request.GET.get('box_size')
 
     img = qrcode.make(request.build_absolute_uri(reverse(f'{view_extras.snake_case(model)}_detail', args=[id])),
@@ -195,6 +282,8 @@ def qr_code_svg(request, model, id):
     return HttpResponse(img.to_string(encoding='unicode'), content_type="image/svg+xml")
 
 
+""" Loop through all model types and create generic list and detail views for them.
+"""
 for name in [obj.__name__ for name, obj in bts.models.__dict__.items()
              if inspect.isclass(obj) and issubclass(obj, models.Model)]:
     generated_class = type(name + 'DetailView', (ModelDetailView, ), {
