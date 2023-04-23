401b68341217d500c3893ffbae6e8a807133d757
nomike <nomike.postmann@paysafe.com>
2023-04-23 15:23:45 +0200

    Document multilevelselect in admin. Use decorators to register admins.

--- bts/admin.py
+++ bts/admin.py
@@ -36,6 +36,49 @@ from django import forms
 
 
 class MultiLevelSelect(Select):
+    """
+     A widget that allows for multiple levels of selection.
+     E.g. if you want to allow the user to select a storage unit compartment, they can select the assortment box first, then the storage unit, and finally the compartment.
+
+     The fields parameter is a JSON string that contains the information about the different levels of selection.
+
+    The format in EBNF grammar is:
+
+     fields = [field, ...]
+     field = [setting, ...]
+     setting = (key, value)
+
+     The keys are:
+         id:             The id of the select element
+         model:          The model that is used for the select element
+         display_field:  The field that is displayed in the select element
+         parent_field:   The field that is used to filter the queryset of the select element
+
+     TODO: "id" could probably be generated out of the model name and might thus be redundant.
+
+     [
+         {
+             'id': 'select-assortment-box',
+             'model': 'AssortmentBox',
+             'display_field': 'name',
+         },
+         {
+             'id': 'select-storage-unit',
+             'model': 'StorageUnit',
+             'display_field': 'number',
+             'parent_field': 'assortment_box_id'
+         },
+         {
+             'id': 'select-storage-unit-compartment',
+             'model': 'StorageUnitCompartment',
+             'display_field': 'name',
+             'parent_field': 'storage_unit_id'
+         }
+     ]
+
+
+    """
+
     def __init__(self, fields):
         self.fields = fields
         super().__init__()
@@ -47,16 +90,19 @@ class MultiLevelSelect(Select):
         return context
 
 
+@admin.register(SubComponent)
 class SubComponentAdmin(admin.ModelAdmin):
     search_fields = ["name", "component_type__name"]
     autocomplete_fields = ["component", "component_type"]
     save_as = True
 
 
+@admin.register(Component)
 class ComponentAdmin(admin.ModelAdmin):
     search_fields = ["part_number", "product_description"]
 
 
+@admin.register(ComponentType)
 class ComponentTypeAdmin(admin.ModelAdmin):
     search_fields = ["name"]
     autocomplete_fields = ["parent"]
@@ -93,6 +139,7 @@ class InventoryAdminForm(forms.ModelForm):
         }
 
 
+@admin.register(Inventory)
 class InventoryAdmin(admin.ModelAdmin):
     autocomplete_fields = ["sub_component"]
     form = InventoryAdminForm
@@ -101,13 +148,9 @@ class InventoryAdmin(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(AssortmentBox)
 admin.site.register(Category)
-admin.site.register(ComponentType, ComponentTypeAdmin)
-admin.site.register(Component, ComponentAdmin)
-admin.site.register(Inventory, InventoryAdmin)
 admin.site.register(Merchant)
 admin.site.register(Purchase)
 admin.site.register(PurchaseLine)
 admin.site.register(StorageUnit)
 admin.site.register(StorageUnitCompartment)
 admin.site.register(StorageUnitType)
-admin.site.register(SubComponent, SubComponentAdmin)
