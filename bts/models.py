a846285f88a43c96da1fd78f431294cef5607669
nomike <nomike.postmann@paysafe.com>
2023-04-23 15:25:53 +0200

    Fix typo in model.

--- bts/models.py
+++ bts/models.py
@@ -135,7 +135,7 @@ class ComponentType(models.Model):
     """
 
     name = models.CharField(max_length=64, unique=True)
-    sparent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
