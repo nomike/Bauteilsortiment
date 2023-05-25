4ccc364de535204f5d21e439a87ac011fa2d91a8
nomike <nomike.postmann@paysafe.com>
2023-05-26 01:51:18 +0200

    Use TitleCase for API urls.

--- bts/urls.py
+++ bts/urls.py
@@ -70,7 +70,7 @@ for object in [
 
     # restfraemwork api
     router.register(
-        r"api/v0/" + snake_case(str(object._meta.verbose_name_plural)),
+        r"api/v0/" + object._meta.verbose_name_plural.title().replace(" ", ""),
         getattr(views, f"{object._meta.object_name}ViewSet"),
     )
 
